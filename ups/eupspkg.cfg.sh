d6417760db7957ee3a0cd1b1280e9cf4eb1fe16c
Tim Jenness <tjenness@lsst.org>
2016-02-12 12:26:59 -0700

    Update to APR v1.5.2
    Explicitly disable the static library as it is not needed
    and the build does not work on OS X with it enabled.

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,5 +1,6 @@
 # EupsPkg config file. Sourced by 'eupspkg'
 
-export CFLAGS="$CFLAGS -fPIC"
-
-CONFIGURE_OPTIONS="--prefix=$PREFIX"
+# Static libraries disabled as they don't seem to build
+# properly on OS X El Capitan and are not used. It does
+# not seem to be worthwhile to disable on OS X only.
+CONFIGURE_OPTIONS="--prefix=$PREFIX --disable-static"
